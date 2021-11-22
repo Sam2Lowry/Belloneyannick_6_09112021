ae6ba803a1a3c6b840f77979a34538dcfbee5a70
Sam2Lowry <yannick.bellone@outlook.fr>
2021-11-22 10:07:34 +0100

    schemas done

--- models/user.js
+++ models/user.js
@@ -1,3 +1,8 @@
-// utilisateur :
-// 		- email
-// 		- password
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({ 
+    email: { type: String, unique:true, required: true },
+    passwordHashed: { type: String, required: true },
+});
+
+module.exports = mongoose.model("user", userSchema);
