349eddfd75014f89826f126b6650ca03de6da58d
Sam2Lowry <yannick.bellone@outlook.fr>
2021-11-22 11:24:30 +0100

    User route in testing

--- app.js
+++ app.js
@@ -31,4 +31,9 @@ app.use((req, res, next) => {
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
+  
 });
+
+app.use("/api/auth", userRoutes);
+// app.use("/api/sauces", sauceRoutes);
+module.exports = app;
---
+++ middlewares/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
+    req.userID = decodedToken.userId;
+    if (req.body.userId && req.body.userId !== req.userID) {
+      throw "Invalid user ID";
+    } else {
+      next();
+    }
+  } catch (error) {
+    return res.status(401).json({
+      message: "Auth failed",
+    });
+  }
+};
