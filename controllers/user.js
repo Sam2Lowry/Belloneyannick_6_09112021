e760833c0c4685a86d851af9d55c673666b377c9
Sam2Lowry <yannick.bellone@outlook.fr>
2021-11-22 10:19:01 +0100

    Modules installed -Bcryp -Jwt -Unique Validator

---
+++ controllers/user.js
@@ -0,0 +1,5 @@
+const user = require("../models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+
--- models/user.js
+++ models/user.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
-
-const userSchema = mongoose.Schema({ 
-    email: { type: String, unique:true, required: true },
-    passwordHashed: { type: String, required: true },
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true, required: true },
+  passwordHashed: { type: String, required: true },
 });
-
+userSchema.plugin(uniqueValidator);
 module.exports = mongoose.model("user", userSchema);
